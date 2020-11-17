abb4ccbeb392ba320e512389812e3d4f6ad3de54
juhasz48 <50230478+juhasz48@users.noreply.github.com>
2020-11-17 11:27:58 +0100

    7. gyakorlat

---
+++ TD1RBH_1022/TD1RBH_7.3.sql
@@ -0,0 +1,19 @@
+CREATE TABLE Tanfolyam
+(tkod INT,
+ar INT,
+tipus VARCHAR(30),
+menevezes VARCHAR(100),
+PRIMARY KEY(tkod));
+
+CREATE TABLE Resztvevo
+(tajszam CHAR(13),
+nev VARCHAR(30),
+lakcim VARCHAR(200),
+PRIMARY KEY(tajszam));
+
+CREATE TABLE Befizetes
+(befizetes INT,
+diak INT,
+kurzus INT,
+FOREIGN KEY(diak) REFERENCES Resztvevo,
+FOREIGN KEY(kurzus) REFERENCES Tanfolyam);
